0a36ceec18e919473baac00bb144a525c1480475
bkantwi <bernardantwi2010@gmail.com>
2022-10-11 14:47:44 +0000

    closed on modal open and close script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
-const modalClos = document.getElementById('close-modal');
+const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
@@ -14,3 +14,7 @@ function showModal() {
 
 // Modal Event Listener
 modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Event listener to close modal outside modal click event
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
